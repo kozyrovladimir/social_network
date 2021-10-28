0d9c4cef6829c39fd825e35b4e656991ec493188
kozyrovladimir <kozyrovladimir@gmail.com>
2021-10-28 13:33:14 +0300

    create body of social network

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <div className="logo-container">
+                    <img src="logo.svg" alt="logo"/>
+                </div>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                     <a href="">Messages</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                     <a href="">Music</a>
+                </div>
+            </nav>
+            <div className="main-content">
+                <div className="head-image-container">
+                </div>
+                <div className="profile">
+                    <div className="profile-img">
+                        <img src="cat.jpg" alt="cat"/>
+                    </div>
+                    <div className="profile-info">
+                        <h2>vladimir</h2>
+                        <p>Age: 26</p>
+                        <p>Hobby: computer games</p>
+                        <p>Working: web-developer</p>
+                    </div>
+                </div>
+                
+            </div>
+            <footer className="footer">
+                <span>2021</span>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
